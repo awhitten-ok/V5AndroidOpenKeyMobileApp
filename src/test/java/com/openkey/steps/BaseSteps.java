58d37bb0e30f454df705d458e4bafc504c3f3137
Monali Rajgor <108866860+mrajgor-ok@users.noreply.github.com>
2023-03-08 02:57:37 +0530

    Update BaseSteps.java

--- src/test/java/com/openkey/steps/BaseSteps.java
+++ src/test/java/com/openkey/steps/BaseSteps.java
@@ -12,16 +12,10 @@ public class BaseSteps extends CapabilitiesManager {
     protected OpenKeyLoginScreen openKeyLoginScreen;
     protected OpenKeyDownloadMobileKeyScreen openKeyDownloadMobileKeyScreen;
     protected OpenKeyDoorLockScreen openDoorLockScreen;
-    protected OpenDoorLockInOfflineMode openDoorLockInOfflineMode;
-    protected OpenKeyShareKeyScreen openKeyShareKeyScreen;
-    protected OpenKeyHamburgerMenuScreen openKeyHamburgerMenuScreen;
     protected OpenKeyMoreMenuScreen openKeyMoreMenuScreen;
     protected OpenKeyWelcomeMessageScreen openKeyWelcomeMessageScreen;
     protected HostPortalLoginScreen hostPortalLogin;
-    protected HostPortalDashboardScreen hostPortalDashboardScreen;
     protected HostPortalCreateSessionScreen hostPortalCreateSessionScreen;
-    protected HostPortalDeleteSessionScreen hostPortalDeleteSessionScreen;
-    protected OpenKeyDeleteSessionPushNotificationInApp openKeyDeleteSessionPushNotificationInApp;
     protected MqttClientClass mqttClient;
 
     public void setupScreens(AndroidDriver driver, MqttClientClass mqttClient) {
@@ -30,16 +24,10 @@ public class BaseSteps extends CapabilitiesManager {
         openKeyLoginScreen = new OpenKeyLoginScreen(driver, mqttClient);
         openKeyDownloadMobileKeyScreen = new OpenKeyDownloadMobileKeyScreen(driver, mqttClient);
         openDoorLockScreen = new OpenKeyDoorLockScreen(driver, mqttClient);
-        openDoorLockInOfflineMode = new OpenDoorLockInOfflineMode(driver, mqttClient);
-        openKeyShareKeyScreen = new OpenKeyShareKeyScreen(driver, mqttClient);
-        openKeyHamburgerMenuScreen = new OpenKeyHamburgerMenuScreen(driver, mqttClient);
         openKeyMoreMenuScreen = new OpenKeyMoreMenuScreen(driver, mqttClient);
         openKeyWelcomeMessageScreen = new OpenKeyWelcomeMessageScreen(driver, mqttClient);
         hostPortalLogin = new HostPortalLoginScreen(driver);
-        hostPortalDashboardScreen = new HostPortalDashboardScreen(driver);
         hostPortalCreateSessionScreen = new HostPortalCreateSessionScreen(driver);
-        hostPortalDeleteSessionScreen = new HostPortalDeleteSessionScreen(driver);
-        openKeyDeleteSessionPushNotificationInApp = new OpenKeyDeleteSessionPushNotificationInApp(driver);
 
     }
 
